49994b470cfdd8a1c98d1e65424e71fa331c77de
kestsaurav21 <kestsaurav21@outlook.com>
2024-02-04 23:39:28 +0530

    feat: room model & api

--- api/routes/rooms.js
+++ api/routes/rooms.js
@@ -1,8 +1,21 @@
 import express, { Router } from "express";
-
-
+import { createRoom, deleteRoom, getRoom, getRooms, updateRoom } from "../controllers/room.js";
+import { verifyAdmin } from "../utils/verifyAdmin.js";
+import { verifyToken } from "../utils/VerifyToken.js";
 const router = express.Router();
 
+//CREATE
+router.post("/:hotelid", verifyToken, verifyAdmin, createRoom)
+//UPDATE
+router.put("/:id", verifyToken, verifyAdmin, updateRoom)
 
+//DELETE
+router.delete("/:id/:hotelid", verifyToken, verifyAdmin, deleteRoom)
+//GET
+
+router.get("/:id", getRoom)
+//GET ALL
+
+router.get("/", getRooms)
 
 export default router;
